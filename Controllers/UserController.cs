656bfa43cd932fbacd8cdc0a0c1b5f44cc54bcf9
ManfordZeng-waikato <zengchang389@gmail.com>
2025-10-12 22:29:06 +1300

    -ModelState

---
+++ Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using LearningScripts.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace LearningScripts.Controllers
+{
+    public class UserController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                //string errors = string.Join("\n", ModelState.Values.SelectMany(value =>
+                //value.Errors).Select(err => err.ErrorMessage).ToList());
+
+                var errors = string.Join("\n",
+                    from v in ModelState.Values
+                    from e in v.Errors
+                    select e.ErrorMessage
+                    );
+
+                //foreach (var value in ModelState.Values)
+                //{
+                //    foreach (var error in value.Errors)
+                //    {
+                //        errorList.Add(error.ErrorMessage);
+                //    }
+                //}
+                return BadRequest(errors);
+            }
+            return Content($"{user}");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningScripts.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "User name can't be null or empty")]
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public double? Price { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"User Object- User name:{UserName}, Email:{Email}," +
+                $" Phone:{Phone}, Passwored:{Password}, Confirm password:{ConfirmPassword}";
+        }
+    }
+}
