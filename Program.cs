8401c4d80807ec0174dd3afc63a824d45db9d808
ManfordZeng-waikato <zengchang389@gmail.com>
2025-10-12 22:29:04 +1300

    ASP.NET CORE Middleware learning

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace LearningScripts
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            //builder.Services.AddTransient<MycustomMiddleware>();
+
             var app = builder.Build();
+            //useWhen example
+            app.UseWhen(
+                context => context.Request.Query.ContainsKey("username"),
+                app =>
+                {
+                    app.Use(async (context, next) =>
+                    {
+                        await context.Response.WriteAsync("Hello from Middleware branch\n");
+                        next();
+                    });
+                }
+                );
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello from Main branch\n");
+            });
 
             //HTTP Requests and Responses
 
@@ -40,13 +59,29 @@ namespace LearningScripts
             //});
 
             //MiddlewareExample
-            app.Run(async (HttpContext context) =>
-            {
-                await context.Response.WriteAsync("hello");
+            //Middleware1
+            //app.Use(async (HttpContext context, RequestDelegate next) =>
+            //{
+            //    await context.Response.WriteAsync("hello\n");
+            //    await next(context);
+            //});
 
+            //Middleware2
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("hello");
+            //    await next();
+            //});
+            //app.UseMiddleware<MycustomMiddleware>();
+            //app.UseMyCustomMiddleware();
+            //app.UseHelloCustomMiddleware();
 
+            //Middleware3
+            //app.Run(async (HttpContext context) =>
+            //{
+            //    await context.Response.WriteAsync("hello again\n");
+            //});
 
-            });
             app.Run();
         }
     }
